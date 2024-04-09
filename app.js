61584619e6b4ca77477d87d5372481598a00a07e
Tiengh <tienguyen231@gmail.com>
2024-04-10 00:01:28 +0700

    create CRUD user api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.js";
 import authRouter from "./routes/auth.js";
 import testRouter from "./routes/test.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/post", postRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
 app.use("/api/test", testRouter);
 
 
